39c238f1079576bdeccbd12475a9d4d5910363eb
Shahidch19 <chshahidofficial119@gmail.com>
2024-09-28 02:49:24 +0500

    Sidebar with Main Navigation added

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import styled from "styled-components"
+import React from 'react';
+import styled from "styled-components";
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
     background-color: var(--color-grey-0);
@@ -7,12 +9,16 @@ const StyledSidebar = styled.aside`
     border-right: 1px solid var(--color-grey-100);
 
     grid-row: 1/-1;
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
 `
 
 function Sidebar() {
   return (
       <StyledSidebar>
-          SIDEBAR
+          <Logo />
+          <MainNav/>
     </StyledSidebar>
   )
 }
