179a23b5e365b53ef3e4a9975d4c3d1df317885c
Shahidch19 <chshahidofficial119@gmail.com>
2024-09-28 00:33:19 +0500

    AppLayout Created

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 
 export default function App() {
@@ -16,14 +17,18 @@ export default function App() {
     <>
     <GlobalStyles/>
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate replace to='dashboard'/>} />
+        <Routes>
+          
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to='dashboard'/>} />
         <Route path='dashboard' element={<Dashboard/> } />
         <Route path='bookings' element={<Bookings/> } />
         <Route path='cabins' element={<Cabins/> } />
         <Route path='users' element={<Users/> } />
         <Route path='settings' element={<Settings/> } />
         <Route path='account' element={<Account/> } />
+        </Route>
+        
         <Route path='login' element={<Login/> } />
         <Route path='*' element={<PageNotFound/> } />
       </Routes>
