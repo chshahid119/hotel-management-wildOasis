ae38a47300b6b4752ee401160e370c1abbebb702
Shahidch19 <shahidchaudhary0729@gmail.com>
2024-10-09 08:03:00 +0500

    add react-hot-toast and adjust AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Account from './pages/Account';
 import Bookings from './pages/Bookings';
@@ -41,7 +42,23 @@ export default function App() {
             <Route path="login" element={<Login />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
+      
+      <Toaster position="top-center" gutter={12} containerStyle={{ margin: '8px' }} toastOptions={{
+        success: {
+          duration: 3000,
+        },
+        errro: {
+          duration: 5000,
+        },
+        style: {
+          fontSize: '16px',
+          maxWidth: '500px',
+          padding: '16px 24px',
+          backgroundColor: 'var(--color-grey-0)',
+          color: 'var(--color-grey-700)',
+        },
+      }} />
       </QueryClientProvider>
   );
 }
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
 import styled from 'styled-components';
 import { deleteCabin } from '../../services/apiCabins';
 import { formatCurrency } from './../../utils/helpers';
@@ -54,13 +55,13 @@ function CabinRow({ cabin }) {
   const { isLoading:isDeleting, mutate }=useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert('Cabin successfully deleted');
-      
+      toast.success('Cabin Successfully Deleted');
+
       queryClient.invalidateQueries({
         queryKey: ['cabins']
       });
     },
-    onError: (err)=>alert(err.message)
+    onError: (err)=>toast.error(err.message)
   })
   return <TableRow role='row'>
     <img src={image} alt={name} />
