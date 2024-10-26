5ba40d57614fe3aed78910037c161b41b1c2e450
chshahid119 <shahidchaudhary0729@gmail.com>
2024-10-27 00:58:29 +0500

    Table Rows with images uploading feature added

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,5 @@
-import supabase from './supabase';
+import supabase, {supabaseUrl} from './supabase';
+
 
 export async function getCabins() {
   let { data, error } = await supabase.from('cabins').select('*');
@@ -11,31 +12,45 @@ export async function getCabins() {
   return data;
 }
 
-export async function createCabin(cabin) {
-  const { data, error } = await supabase.from('cabins').insert([cabin]);
+export async function createCabin(newCabin) {
+    // Bucket Url
+    // https://zxpaxwzbcylqriumgaao.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/", "");
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create a new cabin
+  const { data, error } = await supabase.from('cabins').insert([{ ...newCabin,image: imagePath} ]);
 
   if (error) {
     console.log(error);
     throw new Error('Cabin Could not be created');
   }
 
+  // 2. Upload Image
+  const { error: storageError } = await supabase
+  .storage
+  .from('cabin-images')
+  .upload(imageName, newCabin.image);
+
+  // 3. Delete the Cabin IF there was an error uploading images
+  if(storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.log(storageError);
+    throw new Error('Cabin Image Could not be uploaded and cabin was not created');
+  }
+
+
   return data;
 }
-
-
-
+ 
 export async function deleteCabin(id) {
-const {data, error } = await supabase
-  .from('cabins')
-  .delete()
-  .eq('id', id)
+  const { data, error } = await supabase.from('cabins').delete().eq('id', id);
 
-   if (error) {
+  if (error) {
     console.log(error);
     throw new Error('Cabin Could not be Deleted');
   }
 
   return data;
 }
-
-
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zxpaxwzbcylqriumgaao.supabase.co';
+export const supabaseUrl = 'https://zxpaxwzbcylqriumgaao.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4cGF4d3piY3lscXJpdW1nYWFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0NzU0NjUsImV4cCI6MjA0MzA1MTQ2NX0.gk3en_N2QSsNw0ukXMVaMjNsRCIQwfv0_yRfNOCVYWI';
 const supabase = createClient(supabaseUrl, supabaseKey);
