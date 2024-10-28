882f6388b4bc23c5b53a1c0e1f6e29f5336618a1
chshahid119 <shahidchaudhary0729@gmail.com>
2024-10-28 19:31:04 +0500

    convert Modal into React Portal Modal (totally out of flow of this app dom)

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,4 @@
+import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 import styled from 'styled-components';
 
@@ -51,7 +52,7 @@ const Button = styled.button`
 `;
 
 export default function Modal({ children,onClose }) {
-  return (
+  return createPortal(
     <Overlay>
       <StyledModal>
       <Button onClick={onClose}><HiXMark/></Button>
@@ -59,6 +60,7 @@ export default function Modal({ children,onClose }) {
         {children}
         </div>
       </StyledModal>
-    </Overlay>
+    </Overlay>,
+    document.body
   );
 }
