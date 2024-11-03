65ba7250763b6b8e3d58c851a7cb9bcdb9f1090f
chshahid119 <shahidchaudhary0729@gmail.com>
2024-11-04 04:12:08 +0500

    Build a resueable Context Menu (Imp with Compound Component and Toughest of Course)

--- src/features/cabins/CabinRow-v1.jsx
+++ src/features/cabins/CabinRow-v1.jsx
@@ -7,7 +7,6 @@ import { useCreateCabin } from './useCreateCabin';
 import Modal from '../../ui/Modal';
 import ConfirmDelete from '../../ui/ConfirmDelete';
 import Table from '../../ui/Table';
-import Menus from '../../ui/Menus';
 
 // const TableRow = styled.div`
 //   display: grid;
@@ -20,6 +19,7 @@ import Menus from '../../ui/Menus';
 //     border-bottom: 1px solid var(--color-grey-100);
 //   }
 // `;
+
 
 const Img = styled.img`
   display: block;
@@ -86,34 +86,31 @@ function CabinRow({ cabin }) {
       )}
       <div>
         <Modal>
-          <Menus.Menu>
-            <Menus.Toggle id={cabinId} />
+          <button disabled={isCreating} onClick={handleDuplicate}>
+            <HiSquare2Stack />
+          </button>
 
-            <Menus.List id={cabinId}>
-              <Menus.Button icon={<HiSquare2Stack />} onClick={handleDuplicate}>
-                Duplicate
-              </Menus.Button>
-              <Modal.Open opens="edit">
-                <Menus.Button icon={<HiPencil />}>Edit</Menus.Button>
-              </Modal.Open>
+          <Modal.Open opens="edit">
+            <button>
+              <HiPencil />
+            </button>
+          </Modal.Open>
+          <Modal.Window name="edit">
+            <CreateCabinForm cabinToEdit={cabin} />
+          </Modal.Window>
 
-              <Modal.Open opens="delete">
-                <Menus.Button icon={<HiTrash />}>Delete</Menus.Button>
-              </Modal.Open>
-            </Menus.List>
-
-            <Modal.Window name="edit">
-              <CreateCabinForm cabinToEdit={cabin} />
-            </Modal.Window>
-
-            <Modal.Window name="delete">
-              <ConfirmDelete
-                resourceName="cabin"
-                onConfirm={() => deleteCabin(cabinId)}
-                disabled={isDeleting}
-              />
-            </Modal.Window>
-          </Menus.Menu>
+          <Modal.Open opens="delete">
+            <button>
+              <HiTrash />
+            </button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="cabin"
+              onConfirm={() => deleteCabin(cabinId)}
+              disabled={isDeleting}
+            />
+          </Modal.Window>
         </Modal>
       </div>
     </Table.Row>
--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from '../../ui/Table';
+import Menus from '../../ui/Menus';
 
 const TableHeader = styled.header`
   display: grid;
@@ -28,6 +29,7 @@ export default function CabinTable() {
   
 
   return (
+    <Menus>
     <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
       <Table.Header>
         <div></div>
@@ -39,5 +41,6 @@ export default function CabinTable() {
       </Table.Header>
       <Table.Body data={cabins} render={(cabin=><CabinRow cabin={cabin} key={cabin.id} />)}/>
    </Table>
+   </Menus>
   )
 }
